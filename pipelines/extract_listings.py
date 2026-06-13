543881fbe642886b997ada33c4304995db7d57af
ouffo <fujimotohugo@gmail.com>
2026-06-13 07:52:53 +0200

    add log to check if folder of raw html exists

--- pipelines/extract_listings.py
+++ pipelines/extract_listings.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+from src.utils.logger import logger
 
 from src.ingestion.sources.base import RentalListingSource
 
 def extract_all_listings(source: RentalListingSource):
     folder = Path(source.storage_path)
+    logger.info(f"Folder exists: {folder.exists()}")
+    logger.info(f"Folder: {folder}")
+
     listings = []
     for file_path in folder.glob("*.html"):
         html = file_path.read_text(encoding="utf-8")
